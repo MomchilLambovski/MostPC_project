185ade9f5092511a4ecdef5997dca6a0ac14f2ad
Momchil Lambovski <momchillambovski@gmail.com>
2017-03-19 20:59:57 +0200

    bla

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+// document.getElementById("signUp").addEventListener('click', function replaceMain() {
+//     var mainDivContent = document.body.children[0].children[1];
+//     var newDiv = document.createElement('div');
+//     newDiv.style.backgroundColor = "blue";
+//     newDiv.style.width = "1000px";
+//     newDiv.style.height = "100px";
+//     mainDivContent.innerHTML = newDiv;
+//     // return document.body.children[0].children[1].replaceChild(newDiv, mainDiv);
+// }, false);
+
+// document.body.children[0].children[1].children[0].style.display = "none";
+
